158edd12686f4bcc5084788137326dbbf91c2e16
SEGAL DAN EZEQUIEL <48115720@ort>
2024-05-27 13:58:32 -0300

    Consigna en VS
    EL MAYOR CAMBIO HASTA LA FECHA!!! (me gustan las milanesas)

--- Assets/LoginManager.cs
+++ Assets/LoginManager.cs
@@ -1,6 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+//Cuando el usuario presione el botón "Aceptar"
+//Debe escribirse un mensaje en consola
+//si el texto del Input_Psswrd coincide
+//Con una contraseña predefinida por ustedes
+//escribir "Access Guaranted", Sino escribir "Access Denied"
 
 public class LoginManager : MonoBehaviour
 {
